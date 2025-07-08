0387201f86333a06f7623cd6a4545d9a157b0ea5
mayank <mayankkumars584@gmail.com>
2025-07-08 13:25:20 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt')
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const authmiddleware = require('./authmiddleware');
+
+const mongoose = require('mongoose');
+const app = express()
+mongoose.connect("mongodb+srv://mayankkumars584:Mayank%40146@cluster0.t1lcct0.mongodb.net/coursify-database?retryWrites=true&w=majority")
+const {z}=require("zod")
+
+const { userModel, todoModel } = require("./db");
+app.use(express.json())
+
+const Jwt_SECRET = "macc1234";
+
+app.use("/user",userRouter)
+app.use("/course",courseRouter)
+    
+app.listen(3000)
+
---
+++ backend/routes/course.js
@@ -0,0 +1,10 @@
+const{Router}=require('express')
+const courseRouter=Router()
+
+courseRouter.get("/preview",async(req,res)=>{
+
+})
+
+courseRouter.post("/purchase",async(req,res)=>{
+
+})
